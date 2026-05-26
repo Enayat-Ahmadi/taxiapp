c908f8e493faadd16ca87ad5d2ca71bfd3d8f281
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-05-26 12:38:17 +0200

    edit color and fix them

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <h1 className="mx-auto font-semibold text-4xl">taxi app</h1>
+    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
+      <h1 className="mx-auto font-semibold text-4xl text-teal-active">
+        Taxi app
+      </h1>
     </main>
   );
 }
