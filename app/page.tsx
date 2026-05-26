28b8f83b74ad1da405baaffe518f7bc6c61522f2
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-05-26 13:02:28 +0200

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
-      <h1 className="mx-auto font-semibold text-4xl text-teal-active">
-        Taxi app
-      </h1>
+    <main className="h-screen flex flex-col items-center">
+      <h1 className="mx-auto font-semibold text-4xl">Taxi app</h1>
     </main>
   );
 }
