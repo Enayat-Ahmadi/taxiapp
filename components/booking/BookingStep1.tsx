892b37a49608ad0a21df6f708001c3c054e90448
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-05-28 12:13:02 +0200

    add passenger and bag select

--- components/booking/BookingStep1.tsx
+++ components/booking/BookingStep1.tsx
@@ -3,11 +3,18 @@ import { motion } from "framer-motion";
 import { Card, Input, Button, Select } from "../ui";
 import { getTimeSlots } from "@/lib/utils";
 
+const passenger: string[] = [
+  "1 Passenger",
+  "2 Passengers",
+  "3 Passengers",
+  "4 Passengers",
+];
+const luggage: string[] = ["No Luggage", "1 Bag", "2 Bags", "3 Bags", "4 Bags"];
 export default function BookingStep1() {
   const timeSlots = getTimeSlots();
   return (
     <motion.div
-        initial={{ opacity: 0, y: 20 }}
+      // initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.3 }}
@@ -37,8 +44,21 @@ export default function BookingStep1() {
               }))}
             />
           </div>
+          <div className="md:grid grid-cols-2 gap-4">
+            <Select
+              label="Passengers"
+              options={passenger.map((item) => ({
+                value: item,
+                label: item,
+              }))}
+            />
+            <Select
+              label="Luggage"
+              options={luggage.map((item) => ({ value: item, label: item }))}
+            />
+          </div>
           <Button type="submit" size="lg" className="w-full bg-teal text-white">
-          Vehicle Selection
+            Vehicle Selection
           </Button>
         </form>
       </Card>
