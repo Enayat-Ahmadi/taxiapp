0015f772a2e825887b8eeff723082718178d50e9
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-06-09 11:26:16 +0200

    add bookingStep1 form

--- components/booking/BookingStep1.tsx
+++ components/booking/BookingStep1.tsx
@@ -2,16 +2,33 @@
 import { motion } from "framer-motion";
 import { Card, Input, Button, Select } from "../ui";
 import { getTimeSlots } from "@/lib/utils";
+import { useForm } from "react-hook-form";
 
-const passenger: string[] = [
-  "1 Passenger",
-  "2 Passengers",
-  "3 Passengers",
-  "4 Passengers",
-];
-const luggage: string[] = ["No Luggage", "1 Bag", "2 Bags", "3 Bags", "4 Bags"];
-export default function BookingStep1() {
+type FormFields = {
+  pickupLocation: string;
+  destination: string;
+  phoneNumber: number;
+  date: Date;
+  time: string;
+  passenger: number;
+  luggage: number;
+};
+interface Step1Props {
+  onNext: (data: FormFields) => void;
+  isLoading?: boolean;
+  initialData?: Partial<FormFields>;
+}
+export default function BookingStep1({ onNext, isLoading }: Step1Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>();
   const timeSlots = getTimeSlots();
+
+  function onSubmit(data: FormFields) {
+    console.log(data);
+  }
   return (
     <motion.div
       // initial={{ opacity: 0, y: 20 }}
@@ -22,42 +39,87 @@ export default function BookingStep1() {
       <Card className="mb-6">
         <h2 className="text-2xl font-bold mb-2">Trip Details</h2>
         <p className="mb-6">Tell us where you're going and when</p>
-        <form className="space-y-4">
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
           <div className="space-y-4">
-            <Input label="Pickup Location" placeholder="Enter pickup address" />
-            <Input label="Destination" placeholder="Where to?" />
-          </div>
-          <div>
             <Input
+              {...register("pickupLocation", {
+                required: "pickup location is required",
+              })}
+              label="Pickup Location"
+              placeholder="Enter pickup address"
+              error={errors.pickupLocation?.message}
+            />
+            <Input
+              {...register("destination", {
+                required: "destination is required",
+              })}
+              label="Destination"
+              placeholder="Where to?"
+              error={errors.destination?.message}
+            />
+            <Input
+              {...register("phoneNumber", {
+                required: "phone number is required",
+              })}
               label="Phone Number"
               type="tel"
               placeholder="+49 (123) 123-4567"
+              error={errors.phoneNumber?.message}
             />
           </div>
           <div className="md:grid grid-cols-2 gap-4">
-            <Input type="date" label="Date" className="" />
+            <Input
+              {...register("date", { required: "date is required" })}
+              type="date"
+              label="Date"
+              error={errors.date?.message}
+            />
             <Select
+              {...register("time", {
+                required: "Date must be today or in the future",
+              })}
               label="Time"
               options={timeSlots.map((slot) => ({
                 value: slot,
                 label: slot,
               }))}
+              error={errors.time?.message}
             />
           </div>
           <div className="md:grid grid-cols-2 gap-4">
             <Select
+              {...register("passenger", {
+                required: "At least 1 passenger is required",
+                valueAsNumber: true,
+              })}
               label="Passengers"
-              options={passenger.map((item) => ({
-                value: item,
-                label: item,
+              options={Array.from({ length: 4 }, (_, i) => ({
+                value: i + 1,
+                label: `${i + 1} ${i === 0 ? "Passenger" : "Passengers"}`,
               }))}
+              error={errors.passenger?.message}
             />
             <Select
+              {...register("luggage", {
+                required: "Please select an option",
+                valueAsNumber: true,
+              })}
               label="Luggage"
-              options={luggage.map((item) => ({ value: item, label: item }))}
+              options={Array.from({ length: 4 }, (_, i) => ({
+                value: i,
+                label:
+                  i === 0 ? "No Luggage" : `${i} ${i === 1 ? "Bag" : "Bags"}`,
+              }))}
+              error={errors.luggage?.message}
             />
           </div>
-          <Button type="submit" size="lg" className="w-full bg-teal text-white">
+          <Button
+            type="submit"
+            size="lg"
+            isLoading={isLoading}
+            className="w-full bg-teal text-white"
+          >
             Vehicle Selection
           </Button>
         </form>
