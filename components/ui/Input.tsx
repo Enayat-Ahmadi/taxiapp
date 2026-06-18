410049d524296ceee2dd4f8d0362db6e3a192ecc
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-06-18 09:22:48 +0200

    add id and htmlfor attribute to inputs fields

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -20,6 +20,7 @@ export function Input({ label, type, className, error, ...rest }: InputProps) {
       )}
       <input
         type={type}
+        id={label}
         className={cn(
           "w-full px-4 py-3  rounded-xl border border-slate-700/50 focus:outline-none focus:ring-2 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed appearance-none",
           error && "border-error focus:ring-error/20 focus:border-error",
--- components/ui/Select.tsx
+++ components/ui/Select.tsx
@@ -18,11 +18,15 @@ export function Select({
   return (
     <div>
       {label && (
-        <label className="block text-sm font-medium text-ink-dark mb-2">
+        <label
+          htmlFor={label}
+          className="block text-sm font-medium text-ink-dark mb-2"
+        >
           {label}{" "}
         </label>
       )}
       <select
+        id={label}
         className={cn(
           "w-full px-4 py-3  rounded-xl border border-slate-700/50 focus:outline-none focus:ring-2 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed appearance-none",
           error && "border-error focus:ring-error/20 focus:border-error",
