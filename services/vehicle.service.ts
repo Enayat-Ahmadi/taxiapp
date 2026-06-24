d6e43c51006ea69f6658fdc25dd7b819e9d2c153
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-06-24 10:33:14 +0200

    check if vehicle already exists

--- services/vehicle.service.ts
+++ services/vehicle.service.ts
@@ -1,10 +1,21 @@
 import { connectDB } from "@/lib/db";
 import { Vehicle } from "@/models/vehicle";
 import { VehicleFormData } from "@/lib/validations/vehicles";
+import { toVehicleDto } from "@/lib/utils";
 
 export async function addVehicle(data: VehicleFormData) {
   await connectDB();
-  const vehicle = await Vehicle.create(data);
-  
-  return vehicle.toObject();
+  try {
+    const vehicle = await Vehicle.create(data);
+    return toVehicleDto(vehicle);
+  } catch (error) {
+    if (
+      error instanceof Error &&
+      "code" in error &&
+      (error as { code: unknown }).code === 11000
+    ) {
+      throw new Error("Vehicle already exists");
+    }
+    throw new Error("Failed to create vehicle");
+  }
 }
