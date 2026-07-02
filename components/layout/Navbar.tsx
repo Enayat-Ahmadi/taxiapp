b5a095dafc1741b18b243aab607742b82ac464ec
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-07-02 17:01:18 +0200

    edit header style

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,5 +1,3 @@
-// "use client";
-
 import Link from "next/link";
 import { buttonVariants } from "@/lib/buttonVariants";
 import { ROUTES } from "@/lib/routes";
@@ -8,12 +6,11 @@ import { auth } from "@/auth";
 
 export async function Navbar() {
   const session = await auth();
-  console.log(session);
+
   return (
     <nav className="sticky top-0 z-50 bg-sky backdrop-blur-md border-b border-slate-700/30 safe-top">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 safe-x">
         <div className="flex items-center justify-between h-16">
-          {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
             <div className="w-8 h-8 bg-teal rounded-lg flex items-center justify-center">
               <span className="font-bold text-black">T</span>
@@ -25,7 +22,7 @@ export async function Navbar() {
           <div className="flex gap-2">
             <Link
               href={session ? ROUTES.profile : ROUTES.login}
-              className={buttonVariants("outline", "sm")}
+              className={buttonVariants("outline", "sm", "rounded-full")}
             >
               <User />
             </Link>
