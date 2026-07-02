7d640201bad29089a35ed4de92bdbe05a3a65669
Enayat-Ahmadi <eno.ahmadi22@gmail.com>
2026-07-02 11:47:24 +0200

    edit mongoose schema

--- models/user.ts
+++ models/user.ts
@@ -1,17 +1,37 @@
 import mongoose, { Schema, models, Document } from "mongoose";
 
 interface IUser extends Document {
-  name: string;
+  fullName: string;
   email: string;
   password: string;
   role: "user" | "admin";
 }
-const userSchema = new Schema<IUser>({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
-});
+const userSchema = new Schema<IUser>(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
 export const User =
   models.User<IUser> || mongoose.model<IUser>("User", userSchema);
